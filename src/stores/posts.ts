06cf52d5751ec203d0884ef8922e10eeb378b43f
lucasbear <lucasbear.yo@outlook.com>
2022-10-17 14:11:08 -0500

    Moved to Pinia for State Management

--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,33 +1,16 @@
-import { reactive, readonly } from "vue";
-
-// ref number, string
-// computed
-// reactive {}, Map, Set
-
+import { defineStore } from "pinia";
 interface PostsState {
   foo: string;
 }
 
-export class PostStore {
-  #state: PostsState;
+export const usePosts = defineStore("posts", {
+  state: (): PostsState => ({
+    foo: "foo",
+  }),
 
-  constructor() {
-    this.#state = reactive<PostsState>({
-      foo: "foo",
-    });
-  }
-
-  getState() {
-    return readonly(this.#state);
-  }
-
-  updateFoo(foo: string) {
-    this.#state.foo = foo;
-  }
-}
-
-const store = new PostStore();
-
-export function usePosts() {
-  return store;
-}
+  actions: {
+    updateFoo(foo: string) {
+      this.foo = foo;
+    },
+  },
+});
