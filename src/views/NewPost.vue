82d10a5f65a403089dd953dc8f7af833557b6194
lucasbear <lucasbear.yo@outlook.com>
2022-10-18 15:35:30 -0500

    Created the Post Writer Component

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,1 +1,15 @@
-<template>New Post!</template>
+<script lang="ts" setup>
+import { DateTime } from "luxon";
+import { TimelinePost } from "../posts";
+import PostWriter from "../components/PostWriter.vue";
+
+const post: TimelinePost = {
+  id: "-1",
+  title: "Title",
+  created: DateTime.now(),
+};
+</script>
+<template>
+  New Post!
+  <PostWriter :post="post" />
+</template>
