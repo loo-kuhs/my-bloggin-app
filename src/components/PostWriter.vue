9ee49ff777545fd5986853aa505351462ddcf203
lucasbear <lucasbear.yo@outlook.com>
2022-10-18 16:43:34 -0500

    Reactive Markdown with watchEffect

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, watch, watchEffect } from "vue";
 import { TimelinePost } from "../posts";
+import { marked } from "marked";
 
 const props = defineProps<{
   post: TimelinePost;
@@ -8,7 +9,26 @@ const props = defineProps<{
 
 const title = ref(props.post.title);
 const content = ref(props.post.markdown);
+const html = ref("");
 const contentEditable = ref<HTMLDivElement>();
+
+/* watchEffect(() => {
+  marked.parse(content.value, (err, parseResult) => {
+    html.value = parseResult;
+  });
+}); */
+
+watch(
+  content,
+  (newContent) => {
+    marked.parse(newContent, (err, parseResult) => {
+      html.value = parseResult;
+    });
+  },
+  {
+    immediate: true,
+  }
+);
 
 onMounted(() => {
   if (!contentEditable.value)
@@ -40,6 +60,8 @@ function handleInput() {
     <div class="column">
       <div contenteditable ref="contentEditable" @input="handleInput" />
     </div>
-    <div class="column">{{ content }}</div>
+    <div class="column">
+      <div v-html="html" />
+    </div>
   </div>
 </template>
