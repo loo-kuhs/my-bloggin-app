026a823749913756d595251449838b78df3a2581
lucasbear <lucasbear.yo@outlook.com>
2022-10-19 17:48:30 -0500

    Submited the Form and Event Modifiers

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { computed, ref } from "@vue/reactivity";
 import { validate, length, required } from "../validation";
+import { NewUser } from "../users";
 import FormInput from "./FormInput.vue";
 
 const username = ref("");
@@ -12,12 +13,28 @@ const password = ref("");
 const passwordStatus = computed(() => {
   return validate(password.value, [required, length({ min: 10, max: 40 })]);
 });
+
+const isInvalid = computed(() => {
+  return !usernameStatus.value.valid || !passwordStatus.value.valid;
+});
+
+function handleSubmit() {
+  if (isInvalid.value) return;
+
+  const newUser: NewUser = {
+    username: username.value,
+    password: password.value,
+  };
+
+  console.log(newUser);
+}
 </script>
 
 <template>
-  <form class="form">
+  <form class="form" @submit.prevent="handleSubmit">
     <FormInput name="Username" v-model="username" :status="usernameStatus" />
     <FormInput name="Password" v-model="password" :status="passwordStatus" />
+    <button class="button" :disabled="isInvalid">Submit</button>
   </form>
 </template>
 
