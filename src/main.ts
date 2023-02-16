80459c4c74630760de9b92e0e84fdbb10dcc649e
lucasbear <rukazu89@gmail.com>
2023-02-16 15:08:39 -0600

    Solve the race condition

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import { router } from "./router";
-import App from "./App.vue";
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import App from './App.vue'
+import { router } from './router'
+import { usePosts } from './stores/posts'
+import { useUsers } from './stores/users'
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+
+const app = createApp(App)
+app.use(createPinia())
+
+const usersStore = useUsers()
+const postsStore = usePosts()
+
+Promise.all([
+  usersStore.authenticate(),
+  postsStore.fetchPosts(),
+]).then(() => {
+  app.use(router)
+  app.mount('#app')
+})
